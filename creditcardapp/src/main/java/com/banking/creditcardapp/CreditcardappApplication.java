4516f99dad0242eab60bece75c8400bae4446e62
surendra246 <surendrasonkar@gmail.com>
2025-10-17 12:18:07 +0530

    Inital setup

---
+++ creditcardapp/src/main/java/com/banking/creditcardapp/CreditcardappApplication.java
@@ -0,0 +1,13 @@
+package com.banking.creditcardapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditcardappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditcardappApplication.class, args);
+	}
+
+}
---
+++ creditcardapp/src/test/java/com/banking/creditcardapp/CreditcardappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.creditcardapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditcardappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
